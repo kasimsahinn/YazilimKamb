45d647fbb630a6c145ebaf6482d2ca966fa70a09
Kasım Şahin <19640208007@ogrenci.bartin.edu.tr>
2021-01-22 15:23:32 +0300

    22.01.2021

---
+++ DENEME/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DENEME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("İstanbul");
+            sehirler2.Add("Karabük");
+            sehirler2.Add("Bartın");
+            sehirler2.Add("Erzurum");
+            sehirler2.Add("Sinop");
+            sehirler2.Add("Kastamonu");
+            sehirler2.Add("Ordu");
+
+            Console.WriteLine(sehirler2.Count);
+
+
+        }
+    }
+    class MyList<T>
+    {
+        T[] array;
+        T[] tempArray;
+        
+        public MyList()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            tempArray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+            }
+            array[array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return array.Length; } 
+        }
+
+    }
+}
