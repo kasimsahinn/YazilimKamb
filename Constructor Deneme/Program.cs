37d21a51b39aa6869b906a97e3b0ca829aae9c57
Kasım Şahin <19640208007@ogrenci.bartin.edu.tr>
2021-01-25 09:23:58 +0300

    a

---
+++ Constructor Deneme/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Constructor_Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer = new Customer { Id = 1, FirstName = "Kasım", LastName = "Şahin", City = "İstanbul"};
+
+            Customer customer2 = new Customer(3, "Ayşe", "Şahin", "İstanbul");
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+
+
+            Console.WriteLine(customer.Id);
+        }
+
+    }
+    class Customer
+    {
+
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+    
+}
